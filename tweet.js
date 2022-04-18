7d180fb2a339b427d3f14d7869d5340f8454f9c6
Radi-dev <evaradi18@gmail.com>
2022-04-18 20:14:55 +0100

    change from twit to twitterApi-v2

--- tweet.js
+++ tweet.js
@@ -1,7 +1,7 @@
 //const Twit = require("twit");
 require('dotenv').config();
 
-const {TwitterApi}=require('twitter-api-v2');
+const { TwitterApi } = require('twitter-api-v2');
 //import { TwitterApi } from 'twitter-api-v2';
 
 //initialize twit library with acsess tokens from .env file
@@ -21,20 +21,46 @@ const T = new TwitterApi({
 })
 
 //The tweeting function
-const tweet = (text)=>{
-    const onComplete = (err,reply)=>{
-        if(err){
-            console.log("Error:",err.message);
-        }
-        else{
-            console.log("Success:",reply);
-        }
-    };
-    //console.log(text)
-    //return text
-    T.v2.tweet(text);
+const tweet =async (text) => {
+    await T.v2.tweetThread(splitIntoThread(text));
+
     //T.post("statuses/update",{status:text},onComplete);
 };
+
+
+const splitIntoThread = (_text) => {
+    const text = _text.split(" ");
+    let count = 0;
+    let threshold = 220;
+    let thread = [];
+    let msg = [];
+    for (const [i, item] of text.entries()) {
+        if (i === text.length - 1) {
+            msg.push(item);
+            count += item.length;
+            const _msg = msg.join(" ");
+            thread.push(_msg);
+            msg = [];
+            count = 0;
+            continue;
+        }
+        if (count + item.length > threshold) {
+            msg.push("...");
+            const _msg = msg.join(" ");
+            thread.push(_msg);
+            msg = [];
+            count = 0;
+            msg.push(item);
+            count += item.length;
+            continue;
+        }
+
+        msg.push(item);
+        count += item.length;
+    }
+    return thread;
+};
+
 
 
 module.exports = tweet;
