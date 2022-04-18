4b3c7ee2ae5510b8a813120db5c081b84e345294
Radi-dev <evaradi18@gmail.com>
2022-04-18 20:43:01 +0100

    clean up

--- tweet.js
+++ tweet.js
@@ -1,17 +1,7 @@
-//const Twit = require("twit");
 require('dotenv').config();
 
 const { TwitterApi } = require('twitter-api-v2');
-//import { TwitterApi } from 'twitter-api-v2';
 
-//initialize twit library with acsess tokens from .env file
-
-// const T = new Twit({
-//     consumer_key: process.env.API_KEY
-//     , consumer_secret: process.env.API_KEY_SECRET
-//     , access_token: process.env.ACCESS_TOKEN
-//     , access_token_secret: process.env.ACCESS_TOKEN_SECRET
-// })
 
 const T = new TwitterApi({
     appKey: process.env.API_KEY
@@ -23,22 +13,19 @@ const T = new TwitterApi({
 //The tweeting function
 const tweet =async (text) => {
     await T.v2.tweetThread(splitIntoThread(text));
-    //post as thread
-
-    //T.post("statuses/update",{status:text},onComplete);
 };
 
 
 const splitIntoThread = (_text) => {
     const text = _text.split(" ");
     let count = 0;
-    let threshold = 220;
+    let threshold = 230;
     let thread = [];
     let msg = [];
     for (const [i, item] of text.entries()) {
         if (i === text.length - 1) {
             msg.push(item);
-            count += item.length;
+            count += (item.length + 1); //add extra one to account for spaces during join
             const _msg = msg.join(" ");
             thread.push(_msg);
             msg = [];
@@ -52,16 +39,16 @@ const splitIntoThread = (_text) => {
             msg = [];
             count = 0;
             msg.push(item);
-            count += item.length;
+            count += (item.length + 1);
             continue;
         }
 
         msg.push(item);
-        count += item.length;
+        count += (item.length + 1);
     }
     return thread;
 };
 
-
+//console.log(splitIntoThread("Hello world, this is my first real tweet, and it is not “real”. Last weekend, I took time to learn back-end development using NodeJs after several years of doing so solely in Python(Django). It didn’t take time at all. But to be sure I actually learned anything, I’ve embarked on a personal project to make scheduled tweets using the ExpressJs framework. So, if you’re seeing this text, it means I've successfully made my first POST request to the Twitter API using NodeJs. Currently, the project still has a long way to go and I’ll be making updates to the app over time to add more features. I’ll also be building a python version of the app. The repository for this app can be found at: https://github.com/Radi-dev/tweeter-nodejs#javascript #coding #Nodejs #develope"))
 
 module.exports = tweet;
